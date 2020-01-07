9f3901db9b910f664b7386d852f3d0587003d519
mandeepkhatry <mandeepkhatry1997@gmail.com>
2020-01-07 11:58:07 +0545

    Add application specific type

--- server/kvstore/marshal/marshal.go
+++ server/kvstore/marshal/marshal.go
@@ -3,6 +3,8 @@ package marshal
 import (
 	"encoding/binary"
 	"fmt"
+	"math"
+	"strings"
 	"time"
 )
 
@@ -11,48 +13,87 @@ const (
 	INT_RANGE = int(32768)
 )
 
-
-
 //TypeMarshal takes type of data and value interface as inputs and returns type specific data  byte
 func TypeMarshal(typeOfData string, valueInterface interface{}) []byte {
 	if typeOfData == "int" {
-		buf := make([]byte, 8)
-		//32768 represents range
-		//Make sure to observe this range
-		numToConvert := int(valueInterface.(float64)) + INT_RANGE
-		binary.BigEndian.PutUint32(buf, uint32(numToConvert))
-		return buf
+		return marshalInt(valueInterface)
 
 	} else if typeOfData == "float" {
-		return []byte("TODO")
+		return marshalFloat(valueInterface)
 
 	} else if typeOfData == "string" {
-		return []byte(valueInterface.(string))
+		if len(strings.Split(valueInterface.(string), " ")) > 0 {
+			return marshalWords(valueInterface)
+		}
+		return marshalWords(valueInterface)
 
-	} else if typeOfData == "timestamp" {
-		byteKeyTimestamp, _ := valueInterface.(time.Time).MarshalBinary()
-		return byteKeyTimestamp
+	} else if typeOfData == "time.Time" {
+		return marshalDateTime(valueInterface)
 
 	} else if typeOfData == "bool" {
-		if valueInterface.(bool) == true {
-			return []byte("true")
-		}
-		return []byte("false")
+		return marshalBool(valueInterface)
 
 	}
 	fmt.Println("NEW TYPE OF DATA FOUND")
 	return []byte("New Type")
 }
 
+func marshalInt(valueInterface interface{}) []byte {
+	buf := make([]byte, 8)
+	//32768 represents range
+	//Make sure to observe this range
+	numToConvert := int(valueInterface.(float64)) + INT_RANGE
+	binary.BigEndian.PutUint32(buf, uint32(numToConvert))
+	return buf
+}
 
-//func marshalInt() []byte,error
+func marshalFloat(valueInterface interface{}) []byte {
+	buf := make([]byte, 8)
+	floatNumber := valueInterface.(float64)
+	binary.BigEndian.PutUint64(buf, math.Float64bits(floatNumber))
+	if floatNumber < 0 {
+		for i, _ := range buf {
+			buf[i] = ^buf[i]
+		}
+	} else {
+		buf[0] = buf[0] ^ 0x80
+	}
 
-//func marshalDouble() []byte,error
+	if buf[0] <= 127 {
+		for i, _ := range buf {
+			buf[i] = ^buf[i]
+		}
+		// num := binary.BigEndian.Uint64(buf)
+		// fmt.Println("decoded", math.Float64frombits(num))
+	} else {
+		buf[0] = buf[0] ^ 0x80
+		// num := binary.BigEndian.Uint64(buf)
+		// fmt.Println("decoded", math.Float64frombits(num))
+	}
 
-//func marshalWords() []byte,error //Do this inside main logic
+	return buf
+}
 
-//func marshalBool() []byte,error
+//TODO Not confirmed
+func marshalWord(valueInterface interface{}) []byte {
+	return []byte(valueInterface.(string))
+}
 
-//func marshalDateTime() []byte,error
+//TODO Not confirmed
+func marshalWords(valueInterface interface{}) []byte {
+	return []byte(valueInterface.(string))
 
-//func marshalBlob() []byte,error
+}
+
+func marshalBool(valueInterface interface{}) []byte {
+	if valueInterface.(bool) == true {
+		return []byte("true")
+	}
+	return []byte("false")
+}
+
+func marshalDateTime(valueInterface interface{}) []byte {
+	byteKeyTimestamp, _ := valueInterface.(time.Time).MarshalBinary()
+	return byteKeyTimestamp
+
+}
